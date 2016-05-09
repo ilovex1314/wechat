7a6048f225417a7235d7dbca1340cfc2ad0c2138
ilovex1314 <384574209@qq.com>
2016-05-09 15:34:23 +0800

    day3

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 'use strict'
 
 var koa = require('koa');
-var sha1 = require('sha1');
+var wechat = require('./weichat/g');
+var path = require('path');
+var util = require('./libs/util');
+var wechat_file = path.join(__dirname, './config/accessToken.txt');
 var config = {
 	wechat : {
 		appID : 'wx3c64d68e742077f3',
 		appSecret : '851ee3876a07203f15a23ebce8233c87',
-		token : 'ljh'
+		token : 'ljh',
+		getAccessToken : function(){
+			return util.readFileAsync(wechat_file);
+		},
+		saveAccessToken : function(data){
+			var data = JSON.stringify(data);
+			return util.writeFileAsync(wechat_file, data);
+		}
+
 	}
 }
 
 var app = new koa();
 
-app.use(function *(next){
-	console.log(this.query);
+app.use(wechat(config.wechat));
 
-	var token = config.wechat.token;
-	var signature = this.query.signature;
-	var nonce = this.query.nonce;
-	var timestamp = this.query.timestamp;
-	var echostr = this.query.echostr;
-
-	var str = [token, timestamp, nonce].sort().join('');
-	var sha = sha1(str);
-
-	if(sha === signature){
-		this.body = echostr + '';
-	}else{
-		this.body = 'wrong';
-	}
-})
 
 app.listen(1234);
 console.log('listener');
